cb41cbde3fb091887d7392abf9a14dbb2ebc834f
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-21 20:19:22 +0700

    add SearchParamsNavLink.js

---
+++ src/app/SearchParamsNavLink.js
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+export default function SearchParamsNavLink({ href, children }) {
+	const searchParams = useSearchParams();
+	const isActive =
+		searchParams.get("type") == href.query.type ||
+		(!searchParams.get("type") && href.query.type == "top");
+
+	return (
+		<div className="flex flex-col items-center relative">
+			<Link
+				className={`px-4 py-1 rounded-full ${
+					isActive
+						? "text-orange-500 font-medium"
+						: "text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200"
+				} transition-colors`}
+				href={href}
+			>
+				{children}
+			</Link>
+			{isActive && (
+				<div className="absolute bottom-0 w-1 h-1 bg-orange-500 shadow shadow-orange-500 rounded-full"></div>
+			)}
+		</div>
+	);
+}

3ad3b183b114c40efa6991e6ddeafe7a7686d1bc
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-21 20:19:40 +0700

    update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import BackNavigationButton from "./BackNavigationButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +13,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className="dark scroll-smooth">
 			<body
 				className={`${inter.className} bg-zinc-100 dark:bg-zinc-950 dark:text-zinc-50 relative`}
 			>
-				<header className="bg-white dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-800 p-4 sticky top-0">
-					<Link
-						href="/"
-						className="flex items-center justify-center gap-2 group"
-					>
-						<div className="bg-orange-600 text-white rounded aspect-square w-8 h-8 flex items-center justify-center font-medium select-none group-hover:-rotate-12 transition-all">
-							H
-						</div>
-						<span>Hacker News</span>
-					</Link>
+				<header className="bg-white/50 dark:bg-zinc-900/50 backdrop-blur-md border-b border-zinc-200 dark:border-zinc-800 p-4 sticky top-0 isolate z-50">
+					<div className="md:max-w-[65ch] md:mx-auto flex items-center justify-center relative">
+						<BackNavigationButton />
+						<Link
+							href="/"
+							className="flex items-center justify-center gap-2 w-fit group"
+						>
+							<div className="bg-orange-600 shadow-lg hover:shadow-xl hover:shadow-orange-500/50 shadow-orange-500/50 text-white rounded aspect-square w-8 h-8 flex items-center justify-center font-medium select-none hover:-translate-y-1 transition-all">
+								H
+							</div>
+							<span>Hacker News</span>
+						</Link>
+					</div>
 				</header>
 
 				{children}

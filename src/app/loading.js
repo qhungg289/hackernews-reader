f599b60c125de29b07321e2a67645669c037bbcf
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-21 20:19:48 +0700

    update loading.js

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,3 +1,15 @@
+"use client";
+
+import { CircleNotch } from "@phosphor-icons/react";
+
 export default function Loading() {
-	return <p>Loading...</p>;
+	return (
+		<div className="mt-4 flex justify-center">
+			<div className="bg-white dark:bg-zinc-900 rounded-full border border-zinc-200 dark:border-zinc-800 p-2">
+				<div className="animate-spin">
+					<CircleNotch size={32} style={{ color: "#f97316" }} />
+				</div>
+			</div>
+		</div>
+	);
 }

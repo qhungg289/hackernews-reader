d59cdcee8f64667df6c54ddcf4b29bd9995f679c
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-22 23:16:19 +0700

    update loading state

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,15 +1,45 @@
-"use client";
-
-import { CircleNotch } from "@phosphor-icons/react";
-
 export default function Loading() {
 	return (
-		<div className="mt-4 flex justify-center">
-			<div className="bg-white dark:bg-zinc-900 rounded-full border border-zinc-200 dark:border-zinc-800 p-2">
-				<div className="animate-spin">
-					<CircleNotch size={32} style={{ color: "#f97316" }} />
-				</div>
+		<main>
+			<div className="flex items-center gap-10 p-4 mx-4 my-6 md:max-w-[65ch] md:mx-auto bg-white dark:bg-zinc-900 rounded border border-zinc-200 dark:border-zinc-800 dark:border-t-zinc-600">
+				{[...Array(3)].map((_, i) => (
+					<span
+						key={i}
+						className="px-4 py-2 rounded bg-zinc-600 animate-pulse"
+					></span>
+				))}
 			</div>
-		</div>
+			<div className="bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800 dark:border-t-zinc-600 rounded mx-4 mb-6 md:max-w-[65ch] md:mx-auto divide-y divide-zinc-200 dark:divide-zinc-800">
+				{[...Array(20)].map((_, i) => (
+					<div
+						key={i}
+						className="px-4 py-2 grid grid-cols-[1fr_10fr] animate-pulse"
+					>
+						<div className="flex flex-col justify-center">
+							<span className="bg-zinc-600 w-4 h-4 rounded"></span>
+						</div>
+						<div>
+							<div className="space-x-1">
+								<span className="px-20 py-[0.015rem] rounded bg-zinc-600"></span>
+								<span className="px-8 py-[0.015rem] rounded bg-zinc-600"></span>
+							</div>
+							<div className="mt-1">
+								<span className="px-8 py-[0.015rem] rounded bg-zinc-600">
+									{" "}
+								</span>
+							</div>
+							<div className="mt-2 space-x-3">
+								<span className="px-8 py-[0.015rem] rounded bg-zinc-600">
+									{" "}
+								</span>
+								<span className="px-8 py-[0.015rem] rounded bg-zinc-600">
+									{" "}
+								</span>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</main>
 	);
 }
---
+++ src/app/story/[id]/loading.js
@@ -0,0 +1,29 @@
+export default function Loading() {
+	return (
+		<main>
+			<div className="bg-white dark:bg-zinc-900 rounded border border-zinc-200 dark:border-zinc-800 dark:border-t-zinc-600 mx-4 my-6 md:max-w-[65ch] md:mx-auto p-4 divide-y-2 divide-zinc-200 dark:divide-zinc-800 overflow-auto">
+				<div className="pb-4 animate-pulse">
+					<div className="bg-zinc-600 rounded py-2 px-20 w-3/12"></div>
+					<div className="bg-zinc-600 rounded py-2 px-36 mt-4"></div>
+					<div className="mt-4 flex gap-3">
+						<div className="bg-zinc-600 rounded py-2 w-1/6"></div>
+						<div className="bg-zinc-600 rounded py-2 w-1/6"></div>
+					</div>
+				</div>
+				<div className="animate-pulse">
+					{[...Array(10)].map((_, i) => (
+						<div key={i} className="my-4">
+							<div className="bg-zinc-600 rounded py-[0.35rem] w-1/5"></div>
+							{[...Array(3)].map((_, j) => (
+								<div
+									key={j}
+									className="bg-zinc-600 rounded py-2 my-2"
+								></div>
+							))}
+						</div>
+					))}
+				</div>
+			</div>
+		</main>
+	);
+}

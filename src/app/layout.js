27c8545841745c3dea61b73a234ec19344c396e7
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-23 09:36:28 +0700

    remove shadow from logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
 							href="/"
 							className="flex items-center justify-center gap-2 w-fit group"
 						>
-							<div className="bg-orange-600 shadow-lg hover:shadow-xl hover:shadow-orange-500/50 shadow-orange-500/50 text-white rounded aspect-square w-8 h-8 flex items-center justify-center font-medium select-none hover:-translate-y-1 transition-all">
+							<div className="bg-orange-600 text-white rounded aspect-square w-8 h-8 flex items-center justify-center font-medium select-none hover:-translate-y-1 transition-all">
 								H
 							</div>
 							<span>Hacker News</span>

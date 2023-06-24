26dee4ac6bceb9baebba63d56cd3c69e756cdcdb
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-24 21:03:33 +0700

    update error.js

--- src/app/error.js
+++ src/app/error.js
@@ -8,16 +8,19 @@ export default function Error({ error, reset }) {
 	}, [error]);
 
 	return (
-		<main className="min-h-screen flex flex-col items-center justify-center gap-4">
-			<p className="bg-red-500 text-white px-6 py-4 rounded text-lg">
-				An error occurred: {error.message}
-			</p>
-			<button
-				onClick={() => reset()}
-				className="hover:underline text-orange-500"
-			>
-				Retry
-			</button>
+		<main className="h-full flex flex-col items-center justify-center">
+			<div className="space-y-6 rounded border border-neutral-200 dark:border-neutral-800 w-3/4 max-w-md p-4">
+				<div className="space-y-4">
+					<p className="text-8xl">:(</p>
+					<p>Something went wrong!</p>
+				</div>
+				<button
+					onClick={() => reset()}
+					className="text-orange-500 hover:underline"
+				>
+					Try again
+				</button>
+			</div>
 		</main>
 	);
 }

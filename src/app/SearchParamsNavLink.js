4bb83cbdda7a8372e36017af784c9d073f0189a7
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-25 21:57:30 +0700

    update SearchParamsNavLink.js

--- src/app/SearchParamsNavLink.js
+++ src/app/SearchParamsNavLink.js
@@ -1,18 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
 
 export default function SearchParamsNavLink({ href, children }) {
 	const searchParams = useSearchParams();
+	const pathname = usePathname();
 	const isActive =
-		searchParams.get("type") == href.query.type ||
-		(!searchParams.get("type") && href.query.type == "top");
+		(searchParams.get("type") == href.query.type ||
+			(!searchParams.get("type") && href.query.type == "top")) &&
+		!pathname.startsWith("/story");
 
 	return (
 		<div className="flex flex-col items-center relative">
 			<Link
-				className={`px-4 py-1 ${
+				className={`text-sm ${
 					isActive
 						? "text-orange-500 font-medium"
 						: "text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-200"
@@ -22,7 +24,7 @@ export default function SearchParamsNavLink({ href, children }) {
 				{children}
 			</Link>
 			{isActive && (
-				<div className="absolute bottom-0 w-1 h-1 bg-orange-500 rounded-full"></div>
+				<div className="absolute -bottom-2 w-1 h-1 bg-orange-500 rounded-full"></div>
 			)}
 		</div>
 	);

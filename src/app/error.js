dff4fd6169adfc38c99b28c0559e698f63c35b92
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-25 11:38:13 +0700

    update error.js

--- src/app/error.js
+++ src/app/error.js
@@ -9,7 +9,7 @@ export default function Error({ error, reset }) {
 
 	return (
 		<main className="flex flex-col items-center justify-center">
-			<div className="space-y-8 rounded bg-black border border-neutral-200 dark:border-neutral-800 w-5/6 max-w-md p-4 mt-24">
+			<div className="space-y-8 rounded bg-white dark:bg-black border border-neutral-200 dark:border-neutral-800 w-5/6 max-w-md p-4 mt-24">
 				<div className="space-y-4 font-bold">
 					<p className="text-8xl">:(</p>
 					<p className="text-xl">Something went wrong!</p>

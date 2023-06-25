6adbf34e05a7345220c553df53a3c060d75a74d8
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-25 13:20:55 +0700

    update loading.js

--- src/app/story/[id]/loading.js
+++ src/app/story/[id]/loading.js
@@ -2,7 +2,7 @@ export default function Loading() {
 	return (
 		<main>
 			<div className="bg-white dark:bg-black md:rounded border border-t-0 border-x-0 md:border-t md:border-x border-neutral-200 dark:border-neutral-800 md:mx-auto md:my-6 md:max-w-[80ch] p-4 divide-y-2 divide-neutral-200 dark:divide-neutral-800 overflow-auto">
-				<div className="pb-6 animate-pulse">
+				<div className="pb-4 animate-pulse">
 					<div className="bg-neutral-300 dark:bg-neutral-600 rounded py-2 px-20 w-3/12"></div>
 					<div className="bg-neutral-300 dark:bg-neutral-600 rounded py-2 px-36 mt-4"></div>
 					<div className="mt-4 flex gap-3">
@@ -14,7 +14,7 @@ export default function Loading() {
 					{[...Array(10)].map((_, i) => (
 						<div
 							key={i}
-							className="my-6 pb-6 border-b border-neutral-200 dark:border-neutral-800"
+							className="my-4 pb-4 border-b-2 border-dashed border-neutral-200 dark:border-neutral-800"
 						>
 							<div className="bg-neutral-300 dark:bg-neutral-600 rounded py-[0.35rem] w-1/5"></div>
 							{[...Array(3)].map((_, j) => (

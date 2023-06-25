7677cb6f2edb4d00af5cf3e18fa14bc3014e1973
Hung Duong Quang <qhungg289@protonmail.com>
2023-06-25 22:03:15 +0700

    refactor

---
+++ src/app/Header.js
@@ -0,0 +1,23 @@
+import Logo from "./Logo";
+import SearchParamsNavLink from "./SearchParamsNavLink";
+
+export default function Header() {
+	return (
+		<header className="bg-white dark:bg-black border-b border-neutral-200 dark:border-neutral-800 p-4 sticky top-0 isolate z-50">
+			<div className="md:max-w-[80ch] md:mx-auto flex items-center justify-between md:justify-start gap-8">
+				<Logo />
+				<div className="flex items-center gap-8">
+					<SearchParamsNavLink href={{ query: { type: "top" } }}>
+						Top
+					</SearchParamsNavLink>
+					<SearchParamsNavLink href={{ query: { type: "best" } }}>
+						Best
+					</SearchParamsNavLink>
+					<SearchParamsNavLink href={{ query: { type: "new" } }}>
+						New
+					</SearchParamsNavLink>
+				</div>
+			</div>
+		</header>
+	);
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { Roboto, Roboto_Mono } from "next/font/google";
 import ScrollToTopButton from "./ScrollToTopButton";
-import SearchParamsNavLink from "./SearchParamsNavLink";
-import Logo from "./Logo";
+import Header from "./Header";
 
 const roboto = Roboto({
 	subsets: ["latin"],
@@ -30,28 +29,7 @@ export default function RootLayout({ children }) {
 			<body
 				className={`${roboto.variable} ${roboto_mono.variable} font-sans bg-neutral-100 dark:bg-neutral-950 dark:text-neutral-50 relative h-full`}
 			>
-				<header className="bg-white dark:bg-black border-b border-neutral-200 dark:border-neutral-800 p-4 sticky top-0 isolate z-50">
-					<div className="md:max-w-[80ch] md:mx-auto flex items-center justify-between md:justify-start gap-8">
-						<Logo />
-						<div className="flex items-center gap-8">
-							<SearchParamsNavLink
-								href={{ query: { type: "top" } }}
-							>
-								Top
-							</SearchParamsNavLink>
-							<SearchParamsNavLink
-								href={{ query: { type: "best" } }}
-							>
-								Best
-							</SearchParamsNavLink>
-							<SearchParamsNavLink
-								href={{ query: { type: "new" } }}
-							>
-								New
-							</SearchParamsNavLink>
-						</div>
-					</div>
-				</header>
+				<Header />
 
 				{children}
 
